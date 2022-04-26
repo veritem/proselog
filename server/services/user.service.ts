48cd791dce6734e332bf847a30c8fce3b7f1d5b7
EGOIST <hi@egoist.sh>
2022-04-26 19:27:55 +0800

    tweaks

--- server/services/user.service.ts
+++
@@ -1,7 +0,0 @@
-import { User as DB_User } from "@prisma/client"
-
-export const removeUserPrivateFields = (user: Partial<DB_User>) => {
-  user.email = undefined
-  user.emailVerified = undefined
-  user.apiToken = undefined
-}
